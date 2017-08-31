8830dc0412825ba8b2d683cdd9d488de171569d9
Hung Pham <HNPham@fullsail.edu>
2017-08-31 14:59:31 -0400

    made an start project

---
+++ Graphic2Project/Graphic2Project/stdafx.h
@@ -0,0 +1,36 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+// Windows Header Files:
+#include <windows.h>
+
+// C RunTime Header Files
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+
+
+#include <iostream>
+#include <ctime>
+#include <d3d11.h>
+#include<math.h>
+#pragma comment(lib, "d3d11.lib")
+#include <DirectXMath.h>
+#include <DirectXColors.h>
+#include <DirectXCollision.h>
+#include <DirectXPackedVector.h>
+#include <DirectXPackedVector.h>
+#include<atlbase.h>
+
+using namespace std;
+using namespace DirectX;
+
+// TODO: reference additional headers your program requires here
