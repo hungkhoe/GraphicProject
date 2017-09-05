eca2718d219bb45068ef2bcd2ce95306ef63a893
Hung Pham <HNPham@fullsail.edu>
2017-09-04 23:33:35 -0400

    added vertex shader and pixel shader

--- Graphic2Project/Graphic2Project/Rasterization.h
+++ Graphic2Project/Graphic2Project/Rasterization.h
@@ -10,6 +10,10 @@
 #include <DirectXPackedVector.h>
 #include<atlbase.h>
 
+#include "Trivial_VS.csh"
+#include "Trivial_PS.csh"
+
+
 using namespace std;
 using namespace DirectX;
 // variable
@@ -20,6 +24,57 @@ CComPtr <ID3D11RenderTargetView> RTV;
 CComPtr <ID3D11DeviceContext> context;
 D3D11_VIEWPORT viewport;
 CComPtr <ID3D11Texture2D> pBackBuffer;
+
+ID3D11VertexShader* vertexShader;
+ID3D11PixelShader* pixelShader;
+
+unsigned int num_verts;
+ID3D11Buffer* buffer;
+ID3D11InputLayout* layout;
+
+ID3D11Buffer * constant_buffers;
+
+ID3D11Buffer * cubeBuffer;
+ID3D11Buffer * indexBuffer;
+
+ID3D11Buffer * worldMatrixBuffer;
+ID3D11Buffer * viewMatrixBuffer;
+ID3D11Buffer * projectMatrixBuffer;
+
+XMMATRIX worldMatrix;
+XMMATRIX viewMatrix;
+XMMATRIX projectMatrix;
+
+XMVECTOR Eye = XMVectorSet(0.0f, 3.0f, -6.0f, 0.0f);
+XMVECTOR At = XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
+XMVECTOR Up = XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
+
+//struct
+struct Vert
+{
+	XMFLOAT4 coordination;	
+	XMFLOAT2 uv;
+	XMFLOAT4 color;
+//	XMFLOAT4 normals;
+};
+
+struct WorldMatrix
+{
+	XMMATRIX worldMatrix;
+	XMFLOAT4 meshColor;
+};	
+
+struct ViewMatrix
+{
+	XMMATRIX viewMatrix;
+	
+};
+
+struct ProjectionMatrix
+{
+	XMMATRIX projectMatrix;
+	
+};
 
 
 void SetUpBuffer(HWND window)
@@ -67,14 +122,10 @@ void SetUpBuffer(HWND window)
 	chain->GetBuffer(0, __uuidof(ID3D11Texture2D),
 		(LPVOID*)&pBackBuffer.p);
 
-	// Create a render-target view
+
 	device->CreateRenderTargetView(pBackBuffer, NULL,
 		&RTV.p);
 
-	// Bind the view
-
-
-	// TODO: PART 1 STEP 5
 
 	viewport.Width = rect.right - rect.left;
 	viewport.Height = rect.bottom - rect.top;
@@ -86,12 +137,148 @@ void SetUpBuffer(HWND window)
 	context->OMSetRenderTargets(1, &RTV.p, NULL);
 	context->RSSetViewports(1, &viewport);
 	
+	Vert cube[] =
+	{
+		{ XMFLOAT4(-1.0f, 1.0f, -1.0f,1.0f), XMFLOAT2(1.0f, 0.0f) },
+		{ XMFLOAT4(1.0f, 1.0f, -1.0f,1.0f), XMFLOAT2(0.0f, 0.0f) },
+		{ XMFLOAT4(1.0f, 1.0f, 1.0f,1.0f), XMFLOAT2(0.0f, 1.0f) },
+		{ XMFLOAT4(-1.0f, 1.0f, 1.0f,1.0f), XMFLOAT2(1.0f, 1.0f) },
+
+		{ XMFLOAT4(-1.0f, -1.0f, -1.0f,1.0f), XMFLOAT2(0.0f, 0.0f) },
+		{ XMFLOAT4(1.0f, -1.0f, -1.0f,1.0f), XMFLOAT2(1.0f, 0.0f) },
+		{ XMFLOAT4(1.0f, -1.0f, 1.0f,1.0f), XMFLOAT2(1.0f, 1.0f) },
+		{ XMFLOAT4(-1.0f, -1.0f, 1.0f,1.0f), XMFLOAT2(0.0f, 1.0f) },
+
+		{ XMFLOAT4(-1.0f, -1.0f, 1.0f,1.0f), XMFLOAT2(0.0f, 1.0f) },
+		{ XMFLOAT4(-1.0f, -1.0f, -1.0f,1.0f), XMFLOAT2(1.0f, 1.0f) },
+		{ XMFLOAT4(-1.0f, 1.0f, -1.0f,1.0f), XMFLOAT2(1.0f, 0.0f) },
+		{ XMFLOAT4(-1.0f, 1.0f, 1.0f,1.0f), XMFLOAT2(0.0f, 0.0f) },
+
+		{ XMFLOAT4(1.0f, -1.0f, 1.0f,1.0f), XMFLOAT2(1.0f, 1.0f) },
+		{ XMFLOAT4(1.0f, -1.0f, -1.0f,1.0f), XMFLOAT2(0.0f, 1.0f) },
+		{ XMFLOAT4(1.0f, 1.0f, -1.0f,1.0f), XMFLOAT2(0.0f, 0.0f) },
+		{ XMFLOAT4(1.0f, 1.0f, 1.0f,1.0f), XMFLOAT2(1.0f, 0.0f) },
+
+		{ XMFLOAT4(-1.0f, -1.0f, -1.0f,1.0f), XMFLOAT2(0.0f, 1.0f) },
+		{ XMFLOAT4(1.0f, -1.0f, -1.0f,1.0f), XMFLOAT2(1.0f, 1.0f) },
+		{ XMFLOAT4(1.0f, 1.0f, -1.0f,1.0f), XMFLOAT2(1.0f, 0.0f) },
+		{ XMFLOAT4(-1.0f, 1.0f, -1.0f,1.0f), XMFLOAT2(0.0f, 0.0f) },
+
+		{ XMFLOAT4(-1.0f, -1.0f, 1.0f,1.0f), XMFLOAT2(1.0f, 1.0f) },
+		{ XMFLOAT4(1.0f, -1.0f, 1.0f,1.0f), XMFLOAT2(0.0f, 1.0f) },
+		{ XMFLOAT4(1.0f, 1.0f, 1.0f,1.0f), XMFLOAT2(0.0f, 0.0f) },
+		{ XMFLOAT4(-1.0f, 1.0f, 1.0f,1.0f), XMFLOAT2(1.0f, 0.0f) }
+	};
+
+
+	D3D11_BUFFER_DESC bufferDesc1;
+	ZeroMemory(&bufferDesc1, sizeof(bufferDesc1));
+	bufferDesc1.Usage = D3D11_USAGE_IMMUTABLE;
+	bufferDesc1.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	bufferDesc1.CPUAccessFlags = NULL;
+	bufferDesc1.ByteWidth = sizeof(Vert) * 24;
+
+
+	D3D11_SUBRESOURCE_DATA InitData1;
+	ZeroMemory(&InitData1, sizeof(InitData1));
+	InitData1.pSysMem = cube;	
+
+	device->CreateBuffer(&bufferDesc1, &InitData1, &cubeBuffer);
+
+
+	UINT32 index[]=
+	{
+		3,1,0,
+		2,1,3,
+
+		6,4,5,
+		7,4,6,
+
+		11,9,8,
+		10,9,11,
+
+		14,12,13,
+		15,12,14,
+
+		19,17,16,
+		18,17,19,
+
+		22,20,21,
+		23,20,22
+	};
+
+	bufferDesc1.Usage = D3D11_USAGE_IMMUTABLE;
+	bufferDesc1.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	bufferDesc1.CPUAccessFlags = NULL;
+	bufferDesc1.ByteWidth = sizeof(UINT32) * 36;
+	InitData1.pSysMem = index;
+
+	device->CreateBuffer(&bufferDesc1, &InitData1, &indexBuffer)
+	
+	device->CreateVertexShader(Trivial_VS, sizeof(Trivial_VS), NULL, &vertexShader);
+	device->CreatePixelShader(Trivial_PS, sizeof(Trivial_PS), NULL, &pixelShader);
+	
+	D3D11_INPUT_ELEMENT_DESC vLayout[] =
+	{
+		{ "POSITION", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+		{ "UV", 0, DXGI_FORMAT_R32G32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+		{ "COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 }
+	};
+
+	device->CreateInputLayout(vLayout, 3, Trivial_VS, sizeof(Trivial_VS), &layout);
+	context->IASetInputLayout(layout);
+	
+	bufferDesc1.Usage = D3D11_USAGE_DEFAULT;
+	bufferDesc1.ByteWidth = sizeof(ViewMatrix);
+	bufferDesc1.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+	bufferDesc1.CPUAccessFlags = 0;
+
+	device->CreateBuffer(&bufferDesc1, NULL, &viewMatrixBuffer);
+
+	bufferDesc1.ByteWidth = sizeof(ProjectionMatrix);
+	device->CreateBuffer(&bufferDesc1, NULL, &projectMatrixBuffer);
+
+	bufferDesc1.ByteWidth = sizeof(WorldMatrix);
+	device->CreateBuffer(&bufferDesc1, NULL, &worldMatrixBuffer);
+	
+	worldMatrix = XMMatrixIdentity();
+
+	viewMatrix = XMMatrixLookAtLH(Eye, At, Up);
+
+	ViewMatrix viewMatrixHolder;
+
+	viewMatrixHolder.viewMatrix = XMMatrixTranspose(viewMatrix);
+	context->UpdateSubresource(viewMatrixBuffer, 0, nullptr, &viewMatrixHolder, 0, 0);
+
+	projectMatrix = XMMatrixPerspectiveFovLH(XM_PIDIV4, width / (FLOAT)height, 0.01f, 100.0f);
+
+	ProjectionMatrix projectionHolder;
+	projectionHolder.projectMatrix= XMMatrixTranspose(projectMatrix);
+	context->UpdateSubresource(projectMatrixBuffer, 0, nullptr, &projectionHolder, 0, 0);
+
 }
 
 void Render()
 {
-	//// Just clear the backbuffer
 	context->ClearRenderTargetView(RTV, Colors::DarkBlue);
+
+	context->ClearDepthStencilView(g_pDepthStencilView, D3D11_CLEAR_DEPTH, 1.0f, 0);
+
+	WorldMatrix send_to_vramWorldMatrix;
+	send_to_vramWorldMatrix.worldMatrix = worldMatrix;
+	context->UpdateSubresource(worldMatrixBuffer, 0, nullptr, &send_to_vramWorldMatrix, 0, 0);
+
+
+	g_pImmediateContext->VSSetShader(vertexShader, nullptr, 0);
+	g_pImmediateContext->VSSetConstantBuffers(0, 1, &g_pCBNeverChanges);
+	g_pImmediateContext->VSSetConstantBuffers(1, 1, &g_pCBChangeOnResize);
+	g_pImmediateContext->VSSetConstantBuffers(2, 1, &g_pCBChangesEveryFrame);
+	g_pImmediateContext->PSSetShader(g_pPixelShader, nullptr, 0);
+	g_pImmediateContext->PSSetConstantBuffers(2, 1, &g_pCBChangesEveryFrame);
+	g_pImmediateContext->PSSetShaderResources(0, 1, &g_pTextureRV);
+	g_pImmediateContext->PSSetSamplers(0, 1, &g_pSamplerLinear);
+	g_pImmediateContext->DrawIndexed(36, 0, 0);
+
 	chain->Present(0, 0);
 }
 
--- Graphic2Project/Graphic2Project/Trivial_PS.hlsl
+++ Graphic2Project/Graphic2Project/Trivial_PS.hlsl
@@ -1,5 +1,31 @@
-float4 main( float4 colorFromRasterizer : COLOR ) : SV_TARGET
+
+Texture2D texture1: register(t0);
+SamplerState samplerState1 : register(s0);
+
+struct OUTPUT_VERTEX
 {
-	return colorFromRasterizer;
+	float4 coordinate : POSITION;
+	float2 textureUV : TEXTURE;
+	float4 color : COLOR;
+};
+
+cbuffer FIRST_VRAM : register(b1)
+{
+	matrix View;
 }
 
+cbuffer SECOND_VRAM : register (b2)
+{
+	matrix Projection;
+}
+
+cbuffer THIRD_VRAM : register (b3)
+{
+	matrix World;
+	float4 meshColor;
+}
+
+float4 main(OUTPUT_VERTEX outputRaster) : SV_TARGET
+{
+	return outputRaster.color;
+}
